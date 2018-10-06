86a25ce46c9600d638105ce339223aa4695ac6e8
avisionx <avi17223@iiitd.ac.in>
2018-10-06 07:18:02 +0530

    Help

--- Thing.js
+++ Thing.js
@@ -5,17 +5,17 @@ var Thing = function(x,y,flag){
         alpha -= 15;
         if(flag==1) {
             //image(sprite1, x, y, 50, 50);
-            fill(255,0,255);
+            fill(255,0,255,alpha);
             ellipse(x,y,20,20);
         }
         if(flag==2) {
             //image(sprite2, x, y, 50, 50);
-            fill(255,0,255);
+            fill(255,0,255,alpha);
             ellipse(x,y,20,20);
         }
         if(flag==3) {
             //image(sprite3, x, y, 50, 50);
-            fill(255,0,255);
+            fill(255,0,255,alpha);
             ellipse(x,y,20,20);
         }
         if(alpha < 0){
