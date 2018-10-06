201a4ff801870d2eff7bf09b540b3ab8c1a841e6
avisionx <avi17223@iiitd.ac.in>
2018-10-06 07:13:42 +0530

    Help

--- Thing.js
+++ Thing.js
@@ -1,23 +1,26 @@
 var Thing = function(x,y,flag){
-    var sprite1 = loadImage('1.png');
-    var sprite2 = loadImage('2.png');
-    var sprite3 = loadImage('3.png');
+    var alpha = 255;
     this.gone = false;
     this.show = function(){
+        alpha -= 15;
         if(flag==1) {
             //image(sprite1, x, y, 50, 50);
-            fill(255,255,255);
+            fill(255,0,255);
             ellipse(x,y,20,20);
         }
         if(flag==2) {
             //image(sprite2, x, y, 50, 50);
-            fill(255,255,255);
+            fill(255,0,255);
             ellipse(x,y,20,20);
         }
         if(flag==3) {
             //image(sprite3, x, y, 50, 50);
-            fill(255,255,255);
+            fill(255,0,255);
             ellipse(x,y,20,20);
         }
+        if(alpha < 0){
+        //sets if item is out of view
+        this.gone = true;
+      }
     }
 }
